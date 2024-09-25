89b447bc221995815da57f428ebb992d84e2d099
Henrique <sdbarbosa_haiti@hotmail.com>
2024-09-25 19:43:55 -0300

    Create peradores_relacionais.py
    Operadores relacionais

---
+++ peradores_relacionais.py
@@ -0,0 +1,13 @@
+#sempre que eu utilizar os operadores relacionais eu terei uma resposta booleana
+#pesquisar operador XOR ou !XOR 
+x = 10
+y = 5
+
+print( x > y) #true
+print(x >= y) #true
+print(x < y) #false
+print(x <= y) #false
+print(x == y) #false
+print(x != y) #true
+
+
