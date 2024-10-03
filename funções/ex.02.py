de4c065f85c3c158b924cf7fa180122b9745210d
Henrique Barbosa <140192678+henriquebnascimento@users.noreply.github.com>
2024-10-03 13:50:22 -0300

    Update ex.02.py
    trabalhando com funções

--- funções/ex.02.py
+++ funções/ex.02.py
@@ -1,15 +1,12 @@
-def soma_pares(numeros): # criando uma função que exiba a soma dos números pares encontrados no parametro passado
+def soma_pares(numeros):
     soma = 0
     i = 0
-    pares = 0
     pares_encontrados = []
     while i < len(numeros):
         if numeros[i] % 2 == 0:
             pares_encontrados.append(numeros[i])
-            pares += 1
             soma += numeros[i]
         i += 1
-    print(f'Total de números pares encontrados: {pares}')
-    print(f'Números pares: {pares_encontrados}')
-    print(f'Soma dos números pares encontados: {soma}')
-soma_pares([1,2,3,4,5,6,8,10,21,17,16,])
+    print(f'Pares encontrados: {pares_encontrados}')
+    print(f'A soma dos pares encontrados é: {soma}')
+soma_pares([1,2,3,4,5,6,7,8,9])
